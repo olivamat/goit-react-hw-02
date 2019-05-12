64f60804f247896647e9430fc029f7e07406c431
olivamat <matiasholeh@gmail.com>
2019-05-12 20:24:29 +0300

    Updates

---
+++ precache-manifest.fef9ac613a943577fe02e5c51a4d788e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4cd49f61a8d009c59f3a86bfaeee5cf",
+    "url": "/goit-react-hw-02/index.html"
+  },
+  {
+    "revision": "d545ef621c4701161b00",
+    "url": "/goit-react-hw-02/static/css/main.c1f8c0af.chunk.css"
+  },
+  {
+    "revision": "d515549ca4ce5ab3ae62",
+    "url": "/goit-react-hw-02/static/js/2.fb55a50d.chunk.js"
+  },
+  {
+    "revision": "d545ef621c4701161b00",
+    "url": "/goit-react-hw-02/static/js/main.6ce59c91.chunk.js"
+  },
+  {
+    "revision": "d92f35f1efebc38103b8",
+    "url": "/goit-react-hw-02/static/js/runtime~main.577bbb3b.js"
+  }
+]);
